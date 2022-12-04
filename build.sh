ec17ecbd158a7f2f26e245198c592e8ee08477eb
Elk Cloner <28754537+elkcl@users.noreply.github.com>
2022-12-04 18:21:27 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-~/Development/notebook-generator/bin/notebookgen content --author "Moscow State University: Meanwhile, Elk, MrF \& YaV (Grigorii Bazilevich, Ilia Denisev, Vladimir Yakunin)" --initials "Meanwhile, Elk, MrF \& YaV (Bazilevich, Denisev, Yakunin)" --paper a4paper --columns 3
+~/Development/notebook-generator/bin/notebookgen content --author "Moscow State University: Meanwhile, Elk, MrF \& YaV (Grigorii Bazilevich, Ilia Denisev, Vladimir Yakunin)" --initials "Moscow State University (Bazilevich, Denisev, Yakunin)" --paper a4paper --columns 3
